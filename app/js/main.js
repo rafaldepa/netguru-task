e66442f8c95069324235f66ca86e73c2b2fa9968
sirmovie <kontakt@rafaldepa.pl>
2019-08-29 21:03:27 +0200

    Added javascript for article navigation

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,3 @@
-// Highlight current active article part
-const toggleArticle = () => {
-    let anchors = document.querySelectorAll('.article-navigation__item a');
-    for(let item of anchors) {
-        item.addEventListener('click', () => {
-            [].forEach.call(anchors, (el) => {
-                el.classList.remove("current");
-            });
-            item.classList.add('current');
-        });
-    }   
-}
-
 // Smooth Scrolling to Element
 const initSmoothScroll = (offset = 30) => {
     let anchors = document.querySelectorAll('a[href^="#"]');
@@ -28,10 +15,55 @@ const initSmoothScroll = (offset = 30) => {
     return;
 }
 
+// Highlight current active article part
+const toggleArticle = (hash) => {
+    let anchors = document.querySelectorAll('.article-navigation__item a');
+    [].forEach.call(anchors, (el) => {
+        el.classList.remove("current");
+    });
+    for(let item of anchors) {       
+        if(item.getAttribute('href') == `#${hash}`) {
+            item.classList.add('current');
+        }
+    }   
+}
 
-const initEventHandlers = () => {
-    toggleArticle();
+// Find article parts & offset from top 
+const updateArticleSectionsPosition = () => {
+    const article = document.querySelector('.article');
+    const children = article.children;
+    let anchorPoints = [];
+    for (let el in children) {
+        if(children[el].id) {
+            anchorPoints.push([children[el].id, children[el].offsetTop]);
+        }
+    }
+    return anchorPoints;
+}
+
+// Find current section hash
+const updateActiveSection = (anchorList) => {
+    const scrollY = window.scrollY;
+    let lastItemHash;
+    for (let el in anchorList) {
+        if(scrollY >= anchorList[el][1] - 30) {
+            lastItemHash = anchorList[el][0];
+        }
+    }
+    return toggleArticle(lastItemHash);
+}
+
+
+const initEventListeners = () => {
+    // Active Article Section
+    let articleHooks = updateArticleSectionsPosition();
+    window.addEventListener('resize', () => {
+        articleHooks = updateArticleSectionsPosition();        
+    });
+    window.addEventListener('scroll', () => {
+        updateActiveSection(articleHooks)
+    }); 
 }
 
 initSmoothScroll();
-initEventHandlers();
+initEventListeners();
